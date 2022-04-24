4b272b55aa1452b2501f0dae372183a20c562708
unknown <khashayar.behyar@gmail.com>
2022-04-24 14:59:15 -0400

    uploading assignment

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,45 @@
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	tl.title,
+	tl.from_date,
+	tl.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as tl
+ON (e.emp_no = tl.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no ASC;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
+rt.first_name,
+rt.last_name,
+rt.title
+INTO unique_titles
+FROM retirement_titles as rt
+WHERE to_date = '9999-01-01'
+ORDER BY emp_no ASC, to_date DESC;
+ 
+SELECT COUNT(ut.emp_no),ut.title
+INTO retiring_titles
+FROM unique_titles as ut
+GROUP BY title 
+ORDER BY COUNT(title) DESC;
+
+SELECT DISTINCT ON(e.emp_no)e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	tl.title
+INTO mentorship_eligibilty
+FROM employees AS e
+JOIN dept_emp AS de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles AS tl
+ON (e.emp_no = tl.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND (de.to_date = '9999-01-01')
+ORDER BY emp_no ASC;
